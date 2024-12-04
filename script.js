b81cb5b662b70f2b9386ca88619aa655e67edbb7
Tiago265 <futlastf998@gmail.com>
2024-12-03 21:43:53 -0300

    Update correto do script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function cad(){
     let cadUser = document.getElementById("cadUser").value;
     let cadSenha = document.getElementById("cadSenha").value;
-    const cadUsuario = { cadUser, cadSenha}
-    const bancodeDados = JSON.parse(localStorage.getIem("bancodeDados")) || [];
-    bancodeDados.push(cadUsuario)
-    alert("usuario cadastrado com sucesso!")
-    window.location.href = "index.html"
+    const cadUsuario = { cadUser, cadSenha};
+    const bancodeDados = JSON.parse(localStorage.getItem("bancodeDados")) || [];
+    bancodeDados.push(cadUsuario);
+    localStorage.setItem("bancodeDados", JSON.stringify(bancodeDados));
+    alert("usuario cadastrado com sucesso!");
+    window.location.href = "index.html";
 }
 function logar(){
-    let usuario = { 
-        username:username.value,
-        senha:senha.value
-    }
-    let bancodeDados = JSON.parse(localStorage.getItem("bancodeDados"));
-        if(bancodeDados == null){
-            bancodeDados = [];
+    let username = document.getElementById('username').value;
+    let senha = document.getElementById('senha').value;
+    const bancodeDados = JSON.parse(localStorage.getItem("bancodeDados")) || [];
+    for (let usuario of bancodeDados){
+        if (username == usuario.cadUser && senha == usuario.cadSenha){
+            alert("parabéns, você logou!")
+            break;
         }
-    bancodeDados.push(usuario);
-    localStorage.setItem("bancodeDados", JSON.stringify(bancodeDados));
-    alert("usuário cadastrado com sucesso!");
+    }
 }
 function cadLocation() {
     window.location.href = "cad.html"
